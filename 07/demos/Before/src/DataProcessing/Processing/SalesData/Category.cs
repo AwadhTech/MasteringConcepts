c9c51c28d11b9ba09d6e3a38893567148c04b6b6
Awadh-Awadh <awadhawadh84@yahoo.com>
2023-07-17 10:20:38 +0300

    add techniques for string searching

---
+++ 07/demos/Before/src/DataProcessing/Processing/SalesData/Category.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DataProcessing;
+
+internal readonly struct Category : IEquatable<Category>
+{
+    public static Category Empty = new(string.Empty, string.Empty);
+
+    public Category(string code, string description) => (Code, Description) = (code, description);
+
+    public string Code { get; }
+
+    public string Description { get; }
+
+    public static bool TryParse(string source, [MaybeNullWhen(false)] out Category category)
+    {
+        category = Empty;
+        var colonIndex = source.IndexOf(':');
+        if (colonIndex > -1)
+        {
+            var categoryCode = source[..6];
+            for (var index = 0; index < categoryCode.Length; index++)
+            {
+                if (index < 3 && !char.IsLetter(source, index))
+                    return false;
+                var character = categoryCode[index];
+                if (index >= 3
+                    && character != '0'
+                    && character != '1'
+                    && character != '3')
+                {
+                    return false;
+                }
+            }
+
+            var categoryDescription = source.Substring(colonIndex + 1);
+            category = new Category(categoryCode, categoryDescription);
+            return true;
+        }
+        return false;
+    }
+
+    public static bool TryParseUsingRegex(string source, [MaybeNullWhen(false)] out Category category)
+    {
+        category = Empty;
+        return false;
+    }
+
+    public override bool Equals(object? obj) => obj is Category category && Equals(category);
+
+    public bool Equals(Category other) => Code == other.Code && Description == other.Description;
+
+    public override int GetHashCode() => HashCode.Combine(Code, Description);
+
+    public static bool operator ==(Category left, Category right) => left.Equals(right);
+
+    public static bool operator !=(Category left, Category right) => !(left == right);
+}
---
+++ 07/demos/Before/src/DataProcessing/Processing/SalesData/FailedSalesDataInspector.cs
@@ -0,0 +1,74 @@
+﻿namespace DataProcessing;
+
+internal class FailedSalesDataInspector
+{
+    private readonly ILogger _logger;
+
+    public FailedSalesDataInspector(ProcessingOptions options) => _logger = options.LoggerFactory.CreateLogger<FailedSalesDataInspector>();
+
+    public void InspectAll(IEnumerable<string> failedRows)
+    {
+        foreach (var failedRow in failedRows)
+            Inspect(failedRow);
+    }
+
+    public void Inspect(string failedRow)
+    {
+        ArgumentNullException.ThrowIfNull(failedRow);
+
+        var separatorCount = failedRow.Count(c => c.Equals('|'));
+        if (separatorCount < 6)
+        {
+            _logger.LogWarning("{failedRows} has too few elements", failedRow);
+        }
+
+        if (separatorCount > 6)
+        {
+            _logger.LogWarning("{failedRow} has too many items", failedRow);
+        }
+
+        var lastSeparatorIndex = failedRow.LastIndexOf('|');
+        var categoryColonIndex = failedRow.IndexOf(':', lastSeparatorIndex);
+        if (categoryColonIndex == -1)
+        {
+            _logger.LogWarning("{failedRow} category element is invalid due to a missing colon." , failedRow);
+            return;
+        }
+
+        if (categoryColonIndex == failedRow.Length - 1)
+        {
+            _logger.LogWarning("{FailedRow} category code was expected to followed by a description", failedRow);
+            return;
+        }
+
+        var codeLength = 0;
+        for (int index = lastSeparatorIndex + 1; index < categoryColonIndex; index++)
+        {
+            if (!char.IsWhiteSpace(failedRow[index]))
+            {
+                codeLength++;
+            }
+
+            if (codeLength != 6)
+            {
+                _logger.LogWarning("{FailedRow} category code has invalid length", failedRow);
+                return;
+            }
+        }
+
+        var hasDescription = false;
+        for (var index = categoryColonIndex + 1; index < failedRow.Length; index++)
+        {
+            if (!char.IsWhiteSpace(failedRow[index]))
+            {
+                hasDescription = true;
+                break;
+            }
+        }
+
+        if (!hasDescription)
+        {
+            _logger.LogWarning("{failedRow} has only whitespace in it's description", failedRow);
+        }
+    }
+}
