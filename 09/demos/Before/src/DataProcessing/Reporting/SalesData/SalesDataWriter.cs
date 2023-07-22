58fa9d3fb40c0d1502b46cda038355121bf4eb31
Awadh-Awadh <awadhawadh84@yahoo.com>
2023-07-22 23:34:19 +0300

    applied techniques for string formating and string combinations

---
+++ 09/demos/Before/src/DataProcessing/Reporting/SalesData/SalesDataWriter.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace DataProcessing.Reporting;
+
+internal sealed class SalesDataWriter : DataWriter<IEnumerable<HistoricalSalesData>>
+{
+    public SalesDataWriter(ProcessingOptions options) : base(options)
+    {
+    }
+
+    protected override async Task WriteAsyncCore(
+        string pathAndFileName, 
+        IEnumerable<HistoricalSalesData> data, 
+        CancellationToken cancellationToken = default)
+    {
+        var formattedOutput = string.Empty;
+        foreach (var item in data)
+        {
+            var row = string.Join(',', item.GetFormattedComponents(Options.ApplicationCulture));
+            formattedOutput += string.Concat(row, Environment.NewLine);
+        }
+
+        foreach (var writer in OutputWriters)
+        {
+            await writer.WriteDataAsync(formattedOutput, pathAndFileName, cancellationToken);
+        }
+    }
+}
